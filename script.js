194a5f69c575a325b972084f1ed22bb3e687f74e
JALMEJAY KUMAR SHAW <116828650+The-jalmejay@users.noreply.github.com>
2025-08-28 23:54:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
-//your JS code here. If required.
+//your JS code here. If required.https://www.svgrepo.com/show/345221/three-dots.svg
+let titleInput=document.getElemenhttps://www.svgrepo.com/show/345221/three-dots.svghttps://www.svgrepo.com/show/345221/three-dots.svgtById("title");
+let authorInput=document.getElemenhttps://www.svgrepo.com/show/345221/three-dots.svgtById("author");
+let isbnTnput=document.getElementById("isbn");
+let submitbut=document.getElementById("submit");
+let tablebody=document.getElementById("book-list");
+
+submitbut.addEventListener("click",function(){
+	const title = titleInput.value.trim();
+    const author = authorInput.value.trim();
+    const isbn = isbnInput.value.trim();
+
+	if(title&&author&&isbn){
+		addBookToTable(title,author,isbn)
+		titleInput.value="";
+		authorInput.value="";
+		isbnInput.value="";
+	}else{
+		alert("Please fill all fields !")
+	}
+});
+
+function addBookToTable(title,author,isbn) {
+	let row=document.createElement("tr");
+
+	let titleCell=document.createElement("td");
+	titleCell.textContent=title;
+
+	let authorCell=document.createElement("td");
+	authorCell.textContent=author;
+
+	let isbnCell=document.createElement("td");
+	isbnCell.textContent=isbn;
+
+	let deleteCell=document.createElement("td");
+	let deleteButton=document.createElement("button");
+	deleteButton.textContent="x";
+	deleteButton.setAttribute("class","delete-btn");
+	deleteButton.onclick=()=>{row.remove()};
+
+	deleteCell.appendChild(deleteButton);
+	row.appendChild(titleCell);
+	row.appendChild(authorCell);
+	row.appendChild(isbnCell);
+	row.appendChild(deleteCell);
+	tablebody.appendChild(row);
+}
